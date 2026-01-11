38906ec68358fb268f360e8a7185122d2e210a6b
Robert Myers <myersgroup@gmail.com>
2026-01-11 15:02:25 -0800

    fix(build): Remove invalid prop passed to OverviewPanel

--- app/contacts/[id]/page.tsx
+++ app/contacts/[id]/page.tsx
@@ -168,7 +168,6 @@ export default function ContactDetailPage({
       */}
       <OverviewPanel 
         contact={contact}
-        onEdit={() => setIsEditModalOpen(true)}
       />
 
       {/* MODALS */}
