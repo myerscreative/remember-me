f414d91a28d988a62d921d2beab102f0a403c762
Robert Myers <myersgroup@gmail.com>
2026-01-20 08:48:56 -0800

    Disable PWA temporarily to fix deployment caching issue

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const pwaConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: true, // Temporarily disable PWA to fix deployment caching issue
 });
 
 export default pwaConfig(nextConfig);
